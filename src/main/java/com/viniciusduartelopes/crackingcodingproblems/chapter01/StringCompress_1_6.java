9f298e6f2c04eec4b96a487f88253ab7ab841fef
Vinícius Duarte Lopes <vin.duartelopes@gmail.com>
2021-11-24 20:09:53 -0300

    Adds the problem 1.6 - StringCompress

---
+++ src/main/java/com/viniciusduartelopes/crackingcodingproblems/chapter01/StringCompress_1_6.java
@@ -0,0 +1,33 @@
+package com.viniciusduartelopes.crackingcodingproblems.chapter01;
+
+public class StringCompress_1_6 {
+
+   private static String compress(String source) {
+      int count = 1;
+      char previous = source.charAt(0);
+      StringBuilder sb = new StringBuilder();
+
+      for (int i = 1; i < source.length(); i++) {
+         if (source.charAt(i) == previous) {
+            count++;
+         } else {
+            sb.append(count);
+            sb.append(previous);
+            previous = source.charAt(i);
+            count = 1;
+         }
+      }
+
+      sb.append(count);
+      sb.append(previous);
+
+      return sb.length() < source.length()
+              ? sb.toString() : source;
+   }
+
+   public static void main(String[] args) {
+      System.out.println(compress("aaccccccaaa"));
+      System.out.println(compress("aaccbaaccccaaa"));
+      System.out.println(compress("abcde"));
+   }
+}
