8563452545863f5adbcb7906424e20749b1ee7a9
Vinicius Duarte Lopes <viniciusduarte@ufv.br>
2021-12-01 15:46:19 -0300

    Adds the miniMaxSum problem

---
+++ src/main/java/com/viniciusduartelopes/crackingcodingproblems/hackerrank/MinMaxSum.java
@@ -0,0 +1,38 @@
+package com.viniciusduartelopes.crackingcodingproblems.hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class MinMaxSum {
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long max = arr.get(0);
+        long min = arr.get(0);
+        long sum = 0;
+
+        for (Integer el : arr) {
+            if (el > max) {
+                max = el;
+            } else if (el < min) {
+                min = el;
+            }
+            sum += el;
+        }
+
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
