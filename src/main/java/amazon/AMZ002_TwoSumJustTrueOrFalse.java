64d4fc95994aee6729571263674a81c571d4f27f
Vinícius Duarte Lopes <viniciusduarte@ufv.br>
2021-12-09 18:23:13 -0300

    Correct last

--- src/main/java/amazon/AMZ002_TwoSumJustTrueOrFalse.java
+++ src/main/java/amazon/AMZ002_TwoSumJustTrueOrFalse.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class AMZ002_TwoSumJustTrueOrFalse {
 
-    private static boolean twoSum(int[] numbers, int target) {
+    private static boolean findSumOfTwo(int[] numbers, int target) {
 
         Set<Integer> hs = new HashSet<>();
         Integer diff;
 
         for (int value : numbers) {
-            diff = Math.abs(value - target);
+            diff = target - value;
             if (hs.contains(diff)) {
                 return true;
             }
@@ -22,7 +22,12 @@ public class AMZ002_TwoSumJustTrueOrFalse {
     }
 
     public static void main(String[] args) {
-        System.out.println(twoSum(new int[]{5, 7, 1, 2, 8, 4, 3}, 10));
-        System.out.println(twoSum(new int[]{5, 7, 1, 2, 8, 4, 3}, 19));
+        int[] v = new int[]{5, 7, 1, 2, 8, 4, 3};
+        int[] test = new int[]{3, 20, 1, 2, 7};
+
+        for (int i = 0; i < test.length; i++) {
+            boolean output = findSumOfTwo(v, test[i]);
+            System.out.println("findSumOfTwo(v, " + test[i] + ") = " + (output ? "true" : "false"));
+        }
     }
 }
