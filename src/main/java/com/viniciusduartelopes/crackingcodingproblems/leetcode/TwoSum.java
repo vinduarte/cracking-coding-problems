14e344c5c3319a3490b6c24eedd3264e0c932fc8
Vinícius Duarte Lopes <viniciusduarte@ufv.br>
2021-12-09 17:23:17 -0300

    Add TwoSum leetcode problem

---
+++ src/main/java/com/viniciusduartelopes/crackingcodingproblems/leetcode/TwoSum.java
@@ -0,0 +1,24 @@
+package com.viniciusduartelopes.crackingcodingproblems.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> keyMap = new HashMap<>();
+        Map<Integer, Integer> keyMapIndex = new HashMap<>();
+        Integer diff;
+
+        for (int i = 0; i < nums.length; i++) {
+            diff = target - nums[i];
+            if (keyMap.containsKey(diff)) {
+                return new int[]{keyMapIndex.get(diff), i};
+            }
+            keyMap.put(nums[i], diff);
+            keyMapIndex.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
