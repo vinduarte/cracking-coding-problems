ae48767f1fb999b06b7873e5885eb97b94b54dc0
Vinicius Duarte Lopes <viniciusduarte@ufv.br>
2021-12-28 12:42:42 -0300

    Better solution for twoSum

--- src/main/java/amazon/AMZ002_TwoSumJustTrueOrFalse.java
+++ src/main/java/amazon/AMZ002_TwoSumJustTrueOrFalse.java
@@ -5,28 +5,28 @@ import java.util.Set;
 
 public class AMZ002_TwoSumJustTrueOrFalse {
 
-    private static boolean findSumOfTwo(int[] numbers, int target) {
-        Set<Integer> hs = new HashSet<>();
-        Integer diff;
+  private static boolean findSumOfTwo(int[] numbers, int target) {
+    Set<Integer> hs = new HashSet<>();
+    Integer diff;
 
-        for (int value : numbers) {
-            diff = target - value;
-            if (hs.contains(diff)) {
-                return true;
-            }
-            hs.add(value);
-        }
-
-        return false;
+    for (int value : numbers) {
+      diff = target - value;
+      if (hs.contains(diff)) {
+        return true;
+      }
+      hs.add(value);
     }
 
-    public static void main(String[] args) {
-        int[] v = new int[]{5, 7, 1, 2, 8, 4, 3};
-        int[] test = new int[]{3, 20, 1, 2, 7};
+    return false;
+  }
 
-        for (int i = 0; i < test.length; i++) {
-            boolean output = findSumOfTwo(v, test[i]);
-            System.out.println("findSumOfTwo(v, " + test[i] + ") = " + (output ? "true" : "false"));
-        }
+  public static void main(String[] args) {
+    int[] v = new int[] {5, 7, 1, 2, 8, 4, 3};
+    int[] test = new int[] {3, 20, 1, 2, 7};
+
+    for (int i = 0; i < test.length; i++) {
+      boolean output = findSumOfTwo(v, test[i]);
+      System.out.println("findSumOfTwo(v, " + test[i] + ") = " + (output ? "true" : "false"));
     }
+  }
 }
--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -1,24 +1,49 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> keyMap = new HashMap<>();
-        Map<Integer, Integer> keyMapIndex = new HashMap<>();
-        Integer diff;
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> keyMap = new HashMap<>();
+    Map<Integer, Integer> keyMapIndex = new HashMap<>();
+    Integer diff;
 
-        for (int i = 0; i < nums.length; i++) {
-            diff = target - nums[i];
-            if (keyMap.containsKey(diff)) {
-                return new int[]{keyMapIndex.get(diff), i};
-            }
-            keyMap.put(nums[i], diff);
-            keyMapIndex.put(nums[i], i);
-        }
-
-        return new int[]{};
+    for (int i = 0; i < nums.length; i++) {
+      diff = target - nums[i];
+      if (keyMap.containsKey(diff)) {
+        return new int[] {keyMapIndex.get(diff), i};
+      }
+      keyMap.put(nums[i], diff);
+      keyMapIndex.put(nums[i], i);
     }
+
+    return new int[] {};
+  }
+
+  public static int[] twoSumBetter(int[] nums, int target) {
+    Map<Integer, Integer> keyMap = new HashMap<>();
+    Integer diff;
+
+    for (int i = 0; i < nums.length; i++) {
+      diff = target - nums[i];
+      if (keyMap.containsKey(diff)) {
+        return new int[] {keyMap.get(diff), i};
+      }
+      keyMap.put(nums[i], i);
+    }
+
+    return new int[] {};
+  }
+
+  public static void main(String[] args) {
+    int[] v = new int[] {5, 7, 1, 2, 8, 4, 3};
+    int[] test = new int[] {3, 20, 1, 2, 7};
+
+    for (int i = 0; i < test.length; i++) {
+      System.out.println(Arrays.toString(twoSumBetter(v, test[i])));
+    }
+  }
 }
